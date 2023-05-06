a9a7025b3faf21d5df704d279ef8be5f051376d6
eep-zzx <46120174+eep-zzx@users.noreply.github.com>
2023-05-06 12:04:45 +0800

    Add files via upload

--- sdk/standalone/net_model/scripts/eepbin_cvt.sh
+++ sdk/standalone/net_model/scripts/eepbin_cvt.sh
@@ -1,11 +1,11 @@
  #!/bin/bash
 
-cmd="../eepBinCvt --bin ./scripts/binRoot/yolov4tiny/eeptpu_s2.pub.bin --input ../models/images/ssd/004545.bmp --output header"
+cmd="../eepBinCvt/eepBinCvt --bin ./scripts/binRoot/yolov4tiny/eeptpu_s2.pub.bin --input ../models/images/ssd/004545.bmp --output header"
 
 echo ${cmd}
 eval $cmd
 
-cmd="../eepBinCvt --bin ./scripts/binRoot/yolov4tiny/eeptpu_s2.pub.bin --input ../models/images/ssd/004545.bmp --output mem"
+cmd="../eepBinCvt/eepBinCvt --bin ./scripts/binRoot/yolov4tiny/eeptpu_s2.pub.bin --input ../models/images/ssd/004545.bmp --output mem"
 
 echo ${cmd}
 eval $cmd
